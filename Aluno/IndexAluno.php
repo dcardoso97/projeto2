3aae2c7778ba9f1f4f5c2703d8f143990eee60cd
odacil <odacilpaul@gmail.com>
2017-01-04 17:23:31 +0000

    ve o pq de isso nao esta a meter os estilos

--- Aluno/IndexAluno.php
+++ Aluno/IndexAluno.php
@@ -1,16 +1,9 @@
-<?php 
-if (!isset($_SESSION))
-{
-     session_start();	 
-}
-$user= $_SESSION['username'];
-?>
 <html>
 	<head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
 	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="aluno.css">
+	<link rel="stylesheet" href="admin.css">
 	<head><link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
